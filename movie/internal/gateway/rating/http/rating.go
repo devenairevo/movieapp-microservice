4fd510ad741eb8c0904a2879700397993f034e3f
inajaf <najaf.eldar@gmail.com>
2025-10-14 16:23:42 +0400

    Add Service Discovery

--- movie/internal/gateway/rating/http/rating.go
+++ movie/internal/gateway/rating/http/rating.go
@@ -4,22 +4,32 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
 
 	"movieexample.com/movie/internal/gateway"
+	"movieexample.com/pkg/discovery"
 	"movieexample.com/rating/pkg/model"
 )
 
 type Gateway struct {
-	addr string
+	registry discovery.Registry
 }
 
-func New(addr string) *Gateway {
-	return &Gateway{addr: addr}
+func New(registry discovery.Registry) *Gateway {
+	return &Gateway{registry: registry}
 }
 
+// GetAggregatedRating returns the aggregated rating for a record or ErrNotFound if there are no ratings for it.
 func (g *Gateway) GetAggregatedRating(ctx context.Context, recordID model.RecordID, recordType model.RecordType) (float64, error) {
-	req, err := http.NewRequest(http.MethodGet, g.addr+"/rating", nil)
+	addrs, err := g.registry.ServiceAddresses(ctx, "rating")
+	if err != nil {
+		return 0, err
+	}
+	url := "http://" + addrs[rand.Intn(len(addrs))] + "/rating"
+	log.Printf("Calling rating service. Request: GET " + url)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return 0, err
 	}
@@ -46,13 +56,17 @@ func (g *Gateway) GetAggregatedRating(ctx context.Context, recordID model.Record
 }
 
 // PutRating writes a rating.
-func (g *Gateway) PutRating(ctx context.Context, recordID model.RecordID,
-	recordType model.RecordType, rating *model.Rating) error {
-	req, err := http.NewRequest(http.MethodPut, g.addr+"/rating", nil)
+func (g *Gateway) PutRating(ctx context.Context, recordID model.RecordID, recordType model.RecordType, rating *model.Rating) error {
+	addrs, err := g.registry.ServiceAddresses(ctx, "rating")
 	if err != nil {
 		return err
 	}
-	req = req.WithContext(ctx)
+	url := "http://" + addrs[rand.Intn(len(addrs))] + "/ratng"
+	log.Printf("Calling rating service. Request: PUT " + url)
+	req, err := http.NewRequest(http.MethodPut, addrs[0]+"/rating", nil)
+	if err != nil {
+		return err
+	}
 	values := req.URL.Query()
 	values.Add("id", string(recordID))
 	values.Add("type", fmt.Sprintf("%v", recordType))
